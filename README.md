0d8f54bf77d8608d7d59d05899ae68d5c9b70069
blessingmwiti <blessingmwiti@gmail.com>
2024-08-13 18:24:12 +0300

    updated code to version 2

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
-![Screenshot_20220905-012457~2](https://user-images.githubusercontent.com/88279683/188335764-bfac02b2-cf68-4624-91f0-92e401adf4ad.png)
 # termux-intro
 Changes your termux default welcome message, to a cooler one 😎
 
+## INSTALLATION
 
-INSTALLATION.
-Clone using git clone https://github.com/blessingmwiti/termux-intro.git
-Change directory by typing cd termux-intro 
-Give permissions by typing chmod +x intro.sh
-Run the bash script ./intro.sh
+Clone using 
+
+```bash
+git clone https://github.com/blessingmwiti/termux-intro.git
+```
+
+Change directory by typing 
+
+```bash
+cd termux-intro
+```
+
+Give permissions by typing 
+
+```bash
+chmod +x intro.sh
+```
+
+Run the bash script 
+
+```bash
+./intro.sh
+```
+
 You'll be asked to enter a name, type in you cool name. And click enter.
 Exit then restart Termux to see the new welcome message. Enjoy!
-
-YouTube video on installation, https://youtu.be/Zs32r4084uU
--- intro.sh
+++ intro.sh
@@ -1,19 +1,55 @@
 #!/usr/bin/bash
-echo "Enter name"
+
+# Import necessary modules for colors and animations
+source /data/data/com.termux/files/usr/etc/bash_colors.sh
+source /data/data/com.termux/files/usr/etc/bash_animations.sh
+
+# Function to generate a dynamic and colorful welcome banner
+generate_welcome_banner() {
+    local name=$1
+
+    # Clear the screen
+    clear
+
+    # Display a colorful ASCII banner with dynamic colors and animations
+    echo -e "${BOLD}${CYAN}"
+    echo "********************************************"
+    echo "  WELCOME TO TERMUX, $name! 🌟"
+    echo "********************************************"
+    echo -e "${RESET}"
+
+    # Animate the user's name with a cool color-changing effect
+    animate_text "$name" "figlet" "$BOLD$MAGENTA" "$BOLD$CYAN"
+
+    # Show system info with neofetch, but with custom colors and effects
+    neofetch --ascii_distro termux --colors 4 6 2 3 5 7
+}
+
+# Prompt the user for their name
+echo "Enter your name:"
 read name
-#copies the following code to motd.sh
-echo "echo "★★★★★★★★★★★★★★★★★★★★★★★★★★★★"
-echo "Welcome to Termux $name! 😈"
-echo "★★★★★★★★★★★★★★★★★★★★★★★★★★★★"
-figlet $name
-neofetch" > motd.sh
-#tries to remove motd file if any
-rm /data/data/com.termux/files/usr/etc/motd
-#trie to remove motd.sh file if any
-rm /data/data/com.termux/files/usr/etc/motd.sh
-#moves the created motd.sh file to profile.d directory
-mv motd.sh /data/data/com.termux/files/usr/etc/profile.d 
-pkg install neofetch --yes && pkg install figlet --yes
+
+# Generate and display the welcome banner
+generate_welcome_banner "$name"
+
+# Install the necessary packages for the script to work (if not already installed)
+pkg install neofetch --yes
+pkg install figlet --yes
+pkg install lolcat --yes  # For colorful output
+
+# Remove any existing MOTD files to avoid conflicts
+rm -f /data/data/com.termux/files/usr/etc/motd
+rm -f /data/data/com.termux/files/usr/etc/motd.sh
+
+# Create a new MOTD script with the enhanced welcome message
+echo "#!/usr/bin/bash
+generate_welcome_banner \"$name\"
+" > /data/data/com.termux/files/usr/etc/profile.d/motd.sh
+
+# Make sure the new MOTD script is executable
+chmod +x /data/data/com.termux/files/usr/etc/profile.d/motd.sh
+
+# Clear the screen and notify the user to restart Termux
 clear
-echo "V.1.2"
-echo "Please restart Termux now to confirm changes. Enjoy!"
+echo -e "${BOLD}${GREEN}V.2.0${RESET}"
+echo -e "${BOLD}${YELLOW}Your new Termux experience is ready! Please restart Termux to see the changes. Enjoy!${RESET}"
