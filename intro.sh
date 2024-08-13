fc699fe2cd99be619c5fd283fe2ca785be1e4734
blessingmwiti <blessingmwiti@gmail.com>
2024-08-13 19:25:48 +0300

    hope

--- intro.sh
+++ intro.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-
-
 # Update and install necessary packages
 apt update -y && apt upgrade -y
 apt install neofetch --yes
@@ -23,19 +21,19 @@ rm -f /data/data/com.termux/files/usr/etc/motd.sh
 # Create a new MOTD script with the enhanced welcome message
 cat <<EOF > /data/data/com.termux/files/usr/etc/profile.d/motd.sh
 #!/usr/bin/bash
-GREEN="\e[92m"
-ORANGE="\e[33m"
-BLUE="\e[94m"
-STOP="\e[0m"
-printf "${GREEN}"
-printf "********************************************\n"
-printf "${ORANGE}"
-printf "  WELCOME TO TERMUX, $name! 🌟\n"
-printf "${GREEN}"
-printf "********************************************\n"
-printf "${BLUE}"
+GREEN="\\e[92m"
+ORANGE="\\e[33m"
+BLUE="\\e[94m"
+STOP="\\e[0m"
+printf "\${GREEN}"
+printf "********************************************\\n"
+printf "\${ORANGE}"
+printf "  WELCOME TO TERMUX, $name! 🌟\\n"
+printf "\${GREEN}"
+printf "********************************************\\n"
+printf "\${BLUE}"
 figlet $name
-printf "${STOP}"
+printf "\${STOP}"
 neofetch --ascii_distro termux --colors 4 6 2 3 5 7
 EOF
 
